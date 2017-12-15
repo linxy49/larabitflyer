dfde543f5cd5abdc54ea87326e05dd44b54b3e62
gyougi.rin@rnai.co.jp <gyougi.rin@rnai.co.jp>
2017-12-15 17:09:49 +0900

    Bug fixed

--- src/Linxy49/LaraBitflyer/Client.php
+++ src/Linxy49/LaraBitflyer/Client.php
@@ -27,7 +27,47 @@ class Client
     {
         $response = $this->_client->get(Api::BOARD,[]);
         $json = $response->getBody()->getContents();
-        $array = json_decode($json, true);
+        $result = $this->_format(json_decode($json, true));
+        return $result;
+    }
+
+    private function _format(array $data)
+    {
+        $asks = $this->_lastest($this->_sort($data['asks']), 10, false);
+        $bids = $this->_lastest($this->_sort($data['bids']), 10, true);
+        $result['price'] = $data['mid_price'];
+        $result['asks'] = $asks;
+        $result['bids'] = $bids;
+        return $result;
+    }
+
+    /**
+     *　ソート
+     */
+    private function _sort($array)
+    {
+        uasort($array, function ($a, $b) {
+            if($a['price'] == $b['price']) {
+                return 0;
+            }
+            return ($a['price'] > $b['price']) ? -1 : 1;
+        });
         return $array;
+    }
+
+    /**
+     * 最新の情報を取得する
+     * @param $array
+     * @param $length 件数
+     * @param $type true:先頭 | false:末尾
+     */
+    private function _lastest($array, $length, $type)
+    {
+        if ($type) {
+            return array_slice($array, 0, $length);
+        } else {
+            $count = count($array) - $length;
+            return array_slice($array, $count, $length);
+        }
     }
 }
